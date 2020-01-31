e51effc0e1c5f9eebf98916003fee9202dccc7ab
divaibhav <divaibhav@gmail.com>
2020-02-01 00:58:17 +0530

    Initial commit

---
+++ src/stack/MyStack.java
@@ -0,0 +1,75 @@
+/*
+ *  Created by IntelliJ IDEA.
+ *  User: Vaibhav
+ *  Date: 31-Jan-20
+ *  Time: 1:22 PM
+ */
+package stack;
+
+public class MyStack {
+    private int[] data;
+    private int top;
+    // number of elements in stack
+    private int size;
+
+    public MyStack(int sizeOfArray){
+        data = new int[sizeOfArray];
+        top = 0;
+        size = 0;
+    }
+    // insert new element
+    public void push(int element){
+        if(!isFull()){
+            data[top] = element;
+            top++;
+            size++;
+        }
+        else{
+            System.out.println("Stack OverFlow");
+        }
+    }
+    // helper function
+    public boolean isFull(){
+        boolean response = false;
+        if(top == data.length){
+            response = true;
+        }
+        return response;
+    }
+    //delete last entered item
+    public int pop(){
+        int response = 0;
+        if(!isEmpty()){
+            top--;
+            size--;
+            response = data[top];
+        }
+        else{
+            System.out.println("Stack UnderFlow");
+        }
+        return response;
+    }
+    // checking stack for isEmpty
+    public boolean isEmpty(){
+        boolean response = false;
+        if(top == 0){
+            response = true;
+        }
+        return response;
+    }
+
+    public int peek(){
+        int response = 0;
+        if(!isEmpty()){
+            response = data[top-1];
+        }
+        else{
+            System.out.println("Stack UnderFlow");
+        }
+        return response;
+    }
+    //a method to get number of elements in the stack
+    public int size(){
+        return size;
+    }
+}
